1be24795d86190e745d20fce4231f1212409f3d0
James Farrell <james@toucan.earth>
2023-12-04 13:20:09 +0100

    feat: add telegram bot call with DOTENV

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import chalk from 'chalk';
+import TelegramBot from 'node-telegram-bot-api';
 
 // Define types for the protocol data
 type Protocol = {
@@ -23,6 +25,11 @@ type Narratives = {
 
 // API endpoint
 const API_URL = "https://defillama-datasets.llama.fi/lite/protocols2?b=2";
+
+// Telegram Bot setup (replace with your actual bot token and chat ID)
+const TELEGRAM_BOT_TOKEN = 'YOUR_BOT_TOKEN';
+const TELEGRAM_CHAT_ID = 'YOUR_CHAT_ID';
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
 async function fetch_data(api_url: string): Promise<{ protocols: Protocol[] }> {
     const response = await axios.get(api_url);
@@ -51,7 +58,29 @@ function analyze_data(data: { protocols: Protocol[] }): Narratives {
         });
     });
 
+    // Sort and get top 10 projects in each category
+    for (const category in narratives) {
+        narratives[category].sort((a, b) => b.daily_growth - a.daily_growth);
+        narratives[category] = narratives[category].slice(0, 10);
+    }
+
     return narratives;
+}
+
+function formatNarrativesForTelegram(narratives: Narratives): string {
+    let message = '';
+    for (const category in narratives) {
+        message += `*Category: ${category}*\n`;
+        narratives[category].forEach(protocol => {
+            message += ` - ${protocol.name}: Daily Growth: ${protocol.daily_growth.toFixed(2)}, Weekly Growth: ${protocol.weekly_growth.toFixed(2)}, Monthly Growth: ${protocol.monthly_growth.toFixed(2)}\n`;
+        });
+        message += '\n';
+    }
+    return message;
+}
+
+async function postToTelegram(message: string) {
+    await bot.sendMessage(TELEGRAM_CHAT_ID, message, { parse_mode: 'Markdown' });
 }
 
 async function main() {
@@ -59,16 +88,15 @@ async function main() {
         const data = await fetch_data(API_URL);
         const narratives = analyze_data(data);
 
-        // Print results
+        // Print results with colors
         for (const category in narratives) {
-            console.log(`Category: ${category}`);
+            console.log(chalk.blue(`Category: ${category}`));
             narratives[category].forEach(protocol => {
-                console.log(` - ${protocol.name}: Daily Growth: ${protocol.daily_growth}, Weekly Growth: ${protocol.weekly_growth}, Monthly Growth: ${protocol.monthly_growth}`);
+                console.log(chalk.green(` - ${protocol.name}:`), `Daily Growth: ${protocol.daily_growth.toFixed(2)}, Weekly Growth: ${protocol.weekly_growth.toFixed(2)}, Monthly Growth: ${protocol.monthly_growth.toFixed(2)}`);
             });
         }
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
 
-main();
+        // Format and send message to Telegram
+        const telegramMessage = formatNarrativesForTelegram(narratives);
+        await postToTelegram(telegramMessage);
+        console.log(chalk.yellow('Results posted to Telegram
